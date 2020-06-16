131d584c07c09f59272e80f8f0bf02bf5e513807
Ebenezer Don <ebenezerdonu@gmail.com>
2020-06-16 12:17:59 +0100

    add nav and countdown packages to main class

--- src/main/scala/index.scala
+++ src/main/scala/index.scala
@@ -3,15 +3,19 @@ package main
 import org.scalajs.dom.{document, raw, html }
 import scalatags.JsDom.all._
 import scala.scalajs.js.timers.setInterval
+import nav._
+import countdown._
 
 object Main {
   def main(args: Array[String]): Unit = {
     val content =
       div(cls:="container",
-        "Hello"
+        nav.default,
+        div(id:="timer", 60)
       )
 
     val root = document.getElementById("root")
     root.appendChild(content.render)
+    setInterval(1000) {countdown.timer}
   }
 }
